96df2a6362debee46dd0b92534c39bcacdc5f164
gnhnemre <gunhanemre18@gmail.com>
2023-04-01 21:33:57 +0300

    Update hipotenus.java

--- src/hipotenus.java
+++ src/hipotenus.java
@@ -11,6 +11,7 @@ public class hipotenus {
         float y = scanner.nextFloat();
         double z = Math.sqrt(Math.pow(x,2)+Math.pow(y,2));
         System.out.println("hipotenus ="+z);
+        //asd
         
         
         
