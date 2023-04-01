bb1fd58b62423f35fa1e744e7f96738b0d102848
gnhnemre <gunhanemre18@gmail.com>
2023-04-01 21:31:37 +0300

    Initial commit

---
+++ src/Tutorial0.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+// VKI Hesaplama
+public class Tutorial0 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Kilonuzu Girin");
+        float kg = scanner.nextFloat();
+        System.out.println("Boyunuzu Girin");
+        float m = scanner.nextFloat();
+        float ind = kg/(m*m);
+        System.out.println("VKI degeriniz ="+ ind);
+        
+        
+    
+    
+    
+    
+    }
+    
+}
---
+++ src/hipotenus.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+
+public class hipotenus {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("1. dik kenari girin");
+        float x = scanner.nextFloat();
+        System.out.println("2. dik kenari girin");
+        float y = scanner.nextFloat();
+        double z = Math.sqrt(Math.pow(x,2)+Math.pow(y,2));
+        System.out.println("hipotenus ="+z);
+        
+        
+        
+    }
+    
+    
+    
+    
+}
